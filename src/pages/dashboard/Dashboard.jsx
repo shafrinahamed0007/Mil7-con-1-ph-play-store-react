ee4ab503c289452b8e5a78834be4a69bc4b881dd
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-07-18 18:14:32 +0600

    Rechart setup successfully

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,38 +1,46 @@
-import { Pie, PieChart } from "recharts";
+import { useContext } from "react";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+import { InstallAppsContext } from "../../context/InstalledAppsContext";
+import useApps from "../../hooks/useApps";
 
 const Dashboard = () => {
+  const { installApps } = useContext(InstallAppsContext);
+  const { apps} = useApps();
+  const uninstallApp = apps.length - installApps.length
   const data = [
-    { name: "Group A", value: 400, fill: "#0088fe" },
-    { name: "Group B", value: 400, fill: "#00C49F" },
-    { name: "Group C", value: 300, fill: "#FFBB28" },
-    { name: "Group D", value: 200, fill: "#FF8042" },
+    { name: "Installed", value: installApps.length, fill: "#0088fe" },
+    { name: "UnInstalled", value: uninstallApp, fill: "#00C49F" },
   ];
   return (
-    
-      <div>
-        <PieChart
-          style={{
-            width: "100%",
-            maxWidth: "500px",
-            maxHeight: "80vh",
-            aspectRatio: 1,
-          }}
-          responsive
-        >
-          <Pie
-            data={data}
-            innerRadius="80%"
-            outerRadius="100%"
-            // Corner radius is the rounded edge of each pie slice
-            cornerRadius="50%"
-            fill="#8884d8"
-            // padding angle is the gap between each pie slice
-            paddingAngle={5}
-            dataKey="value"
-            isAnimationActive={true}
-          />
-        </PieChart>
-     
+    <div className="container mx-auto border border-slate-300 my-10 shadow p-10">
+      <h2 className="font-semibold text-3xl mb-16 text-center text-green-500">
+        Installed and uninstalled apps
+      </h2>
+      <PieChart
+        style={{
+          width: "100%",
+          maxWidth: "350px",
+          maxHeight: "80vh",
+          aspectRatio: 1,
+          margin: "auto",
+        }}
+        responsive
+      >
+        <Pie
+          data={data}
+          innerRadius="80%"
+          outerRadius="100%"
+          // Corner radius is the rounded edge of each pie slice
+          cornerRadius="50%"
+          fill="#8884d8"
+          // padding angle is the gap between each pie slice
+          paddingAngle={5}
+          dataKey="value"
+          isAnimationActive={true}
+        />
+        <Legend />
+        <Tooltip />
+      </PieChart>
     </div>
   );
 };
